2617a5b1c22cd6d2a0dbb59cfc2ed945bc63d61c
Yassine Boughroudi <boughroudi21@gmail.com>
2025-02-04 12:18:34 +0100

    git commit -m "Integration tests v1"

---
+++ src/test/java/org/universiapolis/fablab/pfe/healthcareinfra/HealthcareInfraApplicationTests.java
@@ -0,0 +1,13 @@
+package org.universiapolis.fablab.pfe.healthcareinfra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareInfraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
